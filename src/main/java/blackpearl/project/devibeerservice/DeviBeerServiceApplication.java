c6b09585e4ef14c6c8956234a253e6a9009bd17e
devivasamsetti <vasamsetti0928@gmail.com>
2020-04-08 16:14:20 -0400

    Initial project add. closes #1

---
+++ src/main/java/blackpearl/project/devibeerservice/DeviBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package blackpearl.project.devibeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviBeerServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DeviBeerServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/blackpearl/project/devibeerservice/DeviBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package blackpearl.project.devibeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviBeerServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
